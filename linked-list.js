baab0cc2ff6fb955a17b72320acc3df9893d59f7
Pete <peterllombardo@gmail.com>
2023-12-28 11:30:01 -0500

    Added doubly linked list

--- linked-list.js
+++ linked-list.js
@@ -197,5 +197,161 @@ class LinkedList {
   }
   }
 
+   class DoublyLinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+  
+    push(val) {
+      const newNode = new Node(val);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+      this.length++;
+    }
+  
+    unshift(val) {
+      const newNode = new Node(val);
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+      }
+      this.length++;
+    }
+  
+    pop() {
+      if (!this.head) {
+        throw new Error("List is empty");
+      }
+  
+      const removed = this.tail;
+      if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+      }
+      this.length--;
+      return removed.val;
+    }
+  
+    shift() {
+      if (!this.head) {
+        throw new Error("List is empty");
+      }
+  
+      const removed = this.head;
+      if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+        this.head.prev = null;
+      }
+      this.length--;
+      return removed.val;
+    }
+  
+    getAt(idx) {
+      if (idx < 0 || idx >= this.length) {
+        throw new Error("Invalid index");
+      }
+  
+      let current;
+      if (idx < this.length / 2) {
+        current = this.head;
+        for (let i = 0; i < idx; i++) {
+          current = current.next;
+        }
+      } else {
+        current = this.tail;
+        for (let i = this.length - 1; i > idx; i--) {
+          current = current.prev;
+        }
+      }
+  
+      return current.val;
+    }
+  
+    setAt(idx, val) {
+      if (idx < 0 || idx >= this.length) {
+        throw new Error("Invalid index");
+      }
+  
+      const current = this.getAt(idx);
+      current.val = val;
+    }
+  
+    insertAt(idx, val) {
+      if (idx < 0 || idx > this.length) {
+        throw new Error("Invalid index");
+      }
+  
+      if (idx === 0) {
+        this.unshift(val);
+      } else if (idx === this.length) {
+        this.push(val);
+      } else {
+        const newNode = new Node(val);
+        const current = this.getAt(idx);
+        const prevNode = current.prev;
+        newNode.next = current;
+        newNode.prev = prevNode;
+        current.prev = newNode;
+        prevNode.next = newNode;
+        this.length++;
+      }
+    }
+  
+    removeAt(idx) {
+      if (idx < 0 || idx >= this.length) {
+        throw new Error("Invalid index");
+      }
+  
+      if (idx === 0) {
+        return this.shift();
+      } else if (idx === this.length - 1) {
+        return this.pop();
+      } else {
+        const current = this.getAt(idx);
+        const prevNode = current.prev;
+        const nextNode = current.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        this.length--;
+        return current.val;
+      }
+    }
+  
+    average() {
+      if (!this.head) {
+        throw new Error("List is empty");
+      }
+  
+      let current = this.head;
+      let sum = 0;
+  
+      while (current) {
+        sum += current.val;
+        current = current.next;
+      }
+  
+      return sum / this.length;
+    }
+  }
+  
 
-module.exports = LinkedList;
+
+module.exports = {LinkedList, DoublyLinkedList};
